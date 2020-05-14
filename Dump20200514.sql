b73178cf8ee737a168dd1cd1cfc026416d83e4fb
emilielhansen <emilie.lenschau@gmail.com>
2020-05-14 11:27:18 +0200

    database fix

--- Dump20200514.sql
+++ Dump20200514.sql
@@ -25,13 +25,19 @@ DROP TABLE IF EXISTS `billofmaterials`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `billofmaterials` (
-  `materialId` int NOT NULL,
+  `bomId` int NOT NULL AUTO_INCREMENT,
+  `materialId` int DEFAULT NULL,
+  `orderId` int DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
   `length` int DEFAULT NULL,
   `amount` int DEFAULT NULL,
   `unit` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`materialId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`bomId`),
+  KEY `fk_bom_orders_idx` (`orderId`),
+  KEY `fk_bom_materials_idx` (`materialId`),
+  CONSTRAINT `fk_bom_materials` FOREIGN KEY (`materialId`) REFERENCES `constructionmaterials` (`materialId`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `fk_bom_orders` FOREIGN KEY (`orderId`) REFERENCES `orders` (`orderId`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +46,7 @@ CREATE TABLE `billofmaterials` (
 
 LOCK TABLES `billofmaterials` WRITE;
 /*!40000 ALTER TABLE `billofmaterials` DISABLE KEYS */;
-INSERT INTO `billofmaterials` VALUES (1,'25x200 mm. trykimp. Brædt',360,4,'stk.'),(2,'25x200 mm. trykimp. Brædt',540,4,'stk.'),(3,'25x125 mm. trykimp. Brædt',360,2,'stk.'),(4,'25x125 mm. trykimp. Brædt',540,4,'stk'),(5,'38x73 mm. Lægte ubh.',420,1,'stk.'),(6,'45x95 mm. Regular ub.',270,12,'stk.'),(7,'45x95 mm. Regular ub.',240,4,'stk.'),(8,'45x195 mm. spærtræ ubh.',600,2,'stk.'),(9,'45x195 mm. spærtræ ubh.',480,1,'stk.'),(10,'45x195 mm. spærtræ ubh.',600,15,'stk.'),(11,'97x97 mm. trykimp. Stolpe',300,11,'stk.'),(12,'19x100 mm trykimp. Brædt',210,200,'stk.'),(13,'19x100 mm trykimp. Brædt',540,4,'stk.'),(14,'19x100 mm trykimp. Brædt',360,2,'stk.'),(15,'Plastmo Ecolite blåtonet',600,6,'stk.'),(16,'Plastmo Ecolite blåtonet',360,6,'stk.'),(17,'Plastmo bundskruer 200stk',0,3,'pakke'),(18,'Hulbånd 1x20 mm. 10 mtr',0,2,'rulle'),(19,'Universal 190mm højre',0,15,'stk.'),(20,'Universal 190mm venstre',0,15,'stk.'),(21,'4,5 x 60 mm skruer 200 stk',0,1,'pakke'),(22,'4,0 x 50mm beslagskruser 250 stk.',0,3,'pakke'),(23,'bræddebolt 10 x 120mm.',0,18,'stk.'),(24,'Firkantskiver 40x40x11 mm ',0,12,'stk.'),(25,'4,5 x 70 mm. skruer 400 stk',0,2,'pk.'),(26,'4,5 x 50 mm. skruer 300 stk',0,2,'pakke'),(27,'Stalddørsgreb 50x75',0,1,'sæt'),(28,'T-hængsel 390 mm',0,2,'stk.'),(29,'Vinkelbeslag 35',0,32,'stk.');
+INSERT INTO `billofmaterials` VALUES (1,1,1,'25x200 mm. trykimp. Brædt',360,4,'stk.'),(2,2,1,'25x200 mm. trykimp. Brædt',540,4,'stk.'),(3,3,1,'25x125 mm. trykimp. Brædt',360,2,'stk.'),(4,4,1,'25x125 mm. trykimp. Brædt',540,4,'stk'),(5,5,1,'38x73 mm. Lægte ubh.',420,1,'stk.'),(6,6,1,'45x95 mm. Regular ub.',270,12,'stk.'),(7,7,1,'45x95 mm. Regular ub.',240,4,'stk.'),(8,8,1,'45x195 mm. spærtræ ubh.',600,2,'stk.'),(9,9,1,'45x195 mm. spærtræ ubh.',480,1,'stk.'),(10,10,1,'45x195 mm. spærtræ ubh.',600,15,'stk.'),(11,11,1,'97x97 mm. trykimp. Stolpe',300,11,'stk.'),(12,12,1,'19x100 mm trykimp. Brædt',210,200,'stk.'),(13,13,1,'19x100 mm trykimp. Brædt',540,4,'stk.'),(14,14,1,'19x100 mm trykimp. Brædt',360,2,'stk.'),(15,15,1,'Plastmo Ecolite blåtonet',600,6,'stk.'),(16,16,1,'Plastmo Ecolite blåtonet',360,6,'stk.'),(17,17,1,'Plastmo bundskruer 200stk',0,3,'pakke'),(18,18,1,'Hulbånd 1x20 mm. 10 mtr',0,2,'rulle'),(19,19,1,'Universal 190mm højre',0,15,'stk.'),(20,20,1,'Universal 190mm venstre',0,15,'stk.'),(21,21,1,'4,5 x 60 mm skruer 200 stk',0,1,'pakke'),(22,22,1,'4,0 x 50mm beslagskruser 250 stk.',0,3,'pakke'),(23,23,1,'bræddebolt 10 x 120mm.',0,18,'stk.'),(24,24,1,'Firkantskiver 40x40x11 mm ',0,12,'stk.'),(25,25,1,'4,5 x 70 mm. skruer 400 stk',0,2,'pk.'),(26,26,1,'4,5 x 50 mm. skruer 300 stk',0,2,'pakke'),(27,27,1,'Stalddørsgreb 50x75',0,1,'sæt'),(28,28,1,'T-hængsel 390 mm',0,2,'stk.'),(29,29,1,'Vinkelbeslag 35',0,32,'stk.');
 /*!40000 ALTER TABLE `billofmaterials` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,7 +60,7 @@ DROP TABLE IF EXISTS `constructionmaterials`;
 CREATE TABLE `constructionmaterials` (
   `materialId` int NOT NULL,
   `type` varchar(255) DEFAULT NULL,
-  `placement`varchar(255) DEFAULT NULL,
+  `placement` varchar(255) DEFAULT NULL,
   `length` int DEFAULT NULL,
   `unit` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`materialId`)
@@ -72,52 +78,6 @@ INSERT INTO `constructionmaterials` VALUES (1,'25x200 mm. trykimp. Brædt','skur
 UNLOCK TABLES;
 
 --
--- Table structure for table `materials`
---
-
-DROP TABLE IF EXISTS `materials`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `materials` (
-  `kind` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `materials`
---
-
-LOCK TABLES `materials` WRITE;
-/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
-INSERT INTO `materials` VALUES ('Mahogany'),('Gumpole'),('Spruce'),('Birch'),('Oak'),('Pine');
-/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `measurements`
---
-
-DROP TABLE IF EXISTS `measurements`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `measurements` (
-  `Navn` varchar(20) DEFAULT NULL,
-  `measure` int DEFAULT NULL,
-  `unit` varchar(5) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `measurements`
---
-
-LOCK TABLES `measurements` WRITE;
-/*!40000 ALTER TABLE `measurements` DISABLE KEYS */;
-INSERT INTO `measurements` VALUES ('width',270,'cm'),('width',300,'cm'),('width',330,'cm'),('width',360,'cm'),('width',390,'cm'),('width',420,'cm'),('width',480,'cm'),('width',510,'cm'),('width',540,'cm'),('width',570,'cm'),('width',600,'cm'),('width',630,'cm'),('width',660,'cm'),('width',690,'cm'),('width',720,'cm'),('width',750,'cm'),('width',450,'cm'),('width',240,'cm'),('length',240,'cm'),('length',270,'cm'),('length',300,'cm'),('length',330,'cm'),('length',360,'cm'),('length',390,'cm'),('length',420,'cm'),('length',450,'cm'),('length',480,'cm'),('length',510,'cm'),('length',540,'cm'),('length',570,'cm'),('length',600,'cm'),('length',630,'cm'),('length',660,'cm'),('length',690,'cm'),('length',720,'cm'),('length',750,'cm'),('length',780,'cm'),('shed width',210,'cm'),('shed width',240,'cm'),('shed width',270,'cm'),('shed width',300,'cm'),('shed width',330,'cm'),('shed width',360,'cm'),('shed width',390,'cm'),('shed width',420,'cm'),('shed width',450,'cm'),('shed width',480,'cm'),('shed width',510,'cm'),('shed width',540,'cm'),('shed width',570,'cm'),('shed width',600,'cm'),('shed width',630,'cm'),('shed width',660,'cm'),('shed width',690,'cm'),('shed width',720,'cm'),('shed length',150,'cm'),('shed length',180,'cm'),('shed length',210,'cm'),('shed length',240,'cm'),('shed length',270,'cm'),('shed length',300,'cm'),('shed length',330,'cm'),('shed length',360,'cm'),('shed length',390,'cm'),('shed length',420,'cm'),('shed length',450,'cm'),('shed length',480,'cm'),('shed length',510,'cm'),('shed length',540,'cm'),('shed length',570,'cm'),('shed length',600,'cm'),('shed length',630,'cm'),('shed length',660,'cm'),('shed length',690,'cm');
-/*!40000 ALTER TABLE `measurements` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `orders`
 --
 
@@ -125,10 +85,18 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
+  `orderId` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
   `orderdate` date DEFAULT NULL,
-  `orderdelivery` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `orderdelivery` date DEFAULT NULL,
+  `length` int DEFAULT NULL,
+  `width` int DEFAULT NULL,
+  `roofId` int DEFAULT '1',
+  PRIMARY KEY (`orderId`),
+  KEY `fk_roof_orders_idx` (`roofId`),
+  CONSTRAINT `fk_order_roofs` FOREIGN KEY (`roofId`) REFERENCES `roof` (`roofId`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -137,6 +105,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'Jønke','j@j.dk','2020-01-01','2020-02-01',780,600,1);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,8 +117,10 @@ DROP TABLE IF EXISTS `roof`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roof` (
+  `roofId` int NOT NULL,
   `Kind` varchar(20) DEFAULT NULL,
-  `slope` int DEFAULT NULL
+  `slope` int DEFAULT NULL,
+  PRIMARY KEY (`roofId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -159,7 +130,7 @@ CREATE TABLE `roof` (
 
 LOCK TABLES `roof` WRITE;
 /*!40000 ALTER TABLE `roof` DISABLE KEYS */;
-INSERT INTO `roof` VALUES ('sloped',45),('sloped',40),('sloped',35),('sloped',30),('sloped',25),('sloped',20);
+INSERT INTO `roof` VALUES (1,'flat',0),(2,'sloped',25),(3,'sloped',35);
 /*!40000 ALTER TABLE `roof` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -172,4 +143,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-11 14:07:46
+-- Dump completed on 2020-05-14 11:21:02
